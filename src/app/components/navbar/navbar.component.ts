82972d1f35d613b26e2c3b2b89b4517e16816627
Marco Gonzalez <magp2606@gmail.com>
2023-02-27 21:34:47 -0500

    added bg transition on scroll to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  styles: [
+    `
+      .active {
+        background: rgb(0, 0, 0);
+
+        background: -moz-linear-gradient(
+          180deg,
+          rgba(0, 0, 0, 1) 5%,
+          rgba(25, 25, 25, 1) 100%
+        );
+        background: -webkit-linear-gradient(
+          180deg,
+          rgba(0, 0, 0, 1) 5%,
+          rgba(25, 25, 25, 1) 100%
+        );
+        background: linear-gradient(
+          180deg,
+          rgba(0, 0, 0, 1) 5%,
+          rgba(25, 25, 25, 1) 100%
+        );
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#000000",endColorstr="#191919",GradientType=1);
+      }
+    `,
+  ],
 })
 export class NavbarComponent {
   navItems = [
@@ -11,4 +35,12 @@ export class NavbarComponent {
     { name: 'Movies', href: '/movies' },
     { name: 'New & Popular', href: '/latest' },
   ];
+  isScrolled = false;
+
+  @HostListener('window:scroll')
+  scrollEvent() {
+    window.pageYOffset >= 80
+      ? (this.isScrolled = true)
+      : (this.isScrolled = false);
+  }
 }
